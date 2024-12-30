1033347b344860de37174ff6c7969f720b859a36
vindyvalentine <vindyfriendly@gmail.com>
2024-12-30 20:13:15 +0800

    refactor: reorganize code structure and improve error handling

--- atomik_py/atomik.py
+++ atomik_py/atomik.py
@@ -1,59 +1,25 @@
-import io
+from Crypto.PublicKey import RSA
 
 from atomik_py.base import AtomikBase
-from atomik_py.models import SignerGetFileResponse
-from atomik_py.models import SignerSignResponse
-from atomik_py.models import SignerVerifyRespons
-from atomik_py.utils import construct_model
-from atomik_py.utils import prepare_file_for_requests
+from atomik_py.services import Signer
 
 
 class Atomik(AtomikBase):
-    def sign(self, file_stream: io.IOBase):
-        response = self.make_authenticated_request(
-            path="/api/services/signer/sign/",
-            method="POST",
-            files={"file": prepare_file_for_requests(file_stream)},
-        )
-        if error := self.validate_response(response=response):
-            return error
-        response_json = response.json()
-        return construct_model(
-            SignerSignResponse,
-            response=response,
-            response_data={"tx_hash": response_json["tx_hash"]},
-        )
+    def __init__(
+        self,
+        private_key: RSA.RsaKey,
+        client_id: str,
+        client_secret: str,
+        base_url: str,
+        timeout: int = 15,
+    ):
+        self.private_key = private_key
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.base_url = base_url
+        self.access_token = None
+        self.token_expiration = None
+        self.timeout = timeout
 
-    def verify(self, hash):  # noqa: A002
-        response = self.make_authenticated_request(
-            path="/api/services/signer/verify/",
-            method="POST",
-            json={"hash": hash},
-        )
-        if error := self.validate_response(response=response):
-            return error
-        response_json = response.json()
-        return construct_model(
-            SignerVerifyRespons,
-            response=response,
-            response_data={"tx_hash": response_json["tx_hash"]},
-        )
-
-    def get_file(self, hash):  # noqa: A002
-        response = self.make_authenticated_request(
-            path=f"/api/services/signer/files/{hash}/",
-            method="GET",
-        )
-        if error := self.validate_response(response=response):
-            return error
-        response_json = response.json()
-        return construct_model(
-            SignerGetFileResponse,
-            response=response,
-            response_data={
-                "file": response_json["file"],
-                "hash": response_json["hash"],
-                "tx_hash": response_json["tx_hash"],
-                "hash_option": response_json["hash_option"],
-            },
-        )
+        # Assign services
+        self.signer = Signer(self)
---
+++ atomik_py/services/__init__.py
@@ -0,0 +1,1 @@
+from atomik_py.services.signer import Signer  # noqa: F401
